0cb6a8c1579a396e602cbc088c1f52e9f3e86d57
Bill Salefski <Bill@Bill-Salefskis-iMac.local>
2015-02-07 17:10:25 +0200

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Bill Salefski on 07-Feb-15.
+//  Copyright (c) 2015 Bill Salefski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
